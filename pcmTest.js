c8cf455d0da1797ed01998fbac5415cb996eca54
Luke Blanchard <luke.blanchard9@Knights.ucf.edu>
2020-03-14 13:52:25 -0400

    Added wav parser

---
+++ pcmTest.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+const wavefile = require('wavefile');
+
+filename = 'C.wav'
+let wav = new wavefile.WaveFile(fs.readFileSync(filename));
+let samples = wav.getSamples()
+
+// Print out the first n samples in the .wav file
+n = 5000
+for (i = 0; i < n; i++) {
+    console.log(samples[i])
+}
