8c409176dabf8e6a90974596891798f885cb1952
Luke Blanchard <luke.blanchard9@Knights.ucf.edu>
2020-03-16 14:01:01 -0400

    Added temp mixer

--- src/mixer.js
+++ src/mixer.js
@@ -4,4 +4,16 @@
 // integer shorts to represent 16-bit PCM audio.
 // The lists may be of arbitrary length as long as they are
 // all the same length as each other.
+const mix = function() {
+    // Pull the audio streams from the function arguments object
+    var streams = arguments
+    // Loop through the streams and print them
+    for (i = 0; i < streams.length; i++) {
+        console.log(streams[i])
+    }
+}
 
+// 2 audio streams with 2 samples each
+var stream1 = new Int16Array([1, 2])
+var stream2 = new Int16Array([3, 4])
+mix(stream1,stream2)
