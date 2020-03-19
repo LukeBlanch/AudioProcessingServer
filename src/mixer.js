b74df8fc988abc22e7a7ca8a6a3d8e6a3fe8a36b
LukeBlanch <luke.blanchard9@knights.ucf.edu>
2020-03-19 10:47:30 -0400

    Added basic functionality to mixer

--- src/mixer.js
+++ src/mixer.js
@@ -1,5 +1,6 @@
 // This function takes an arbitrary number of audio streams and 
-// mixes them together into a single output stream.
+// mixes them together into a single output stream by adding all 
+// the samples in each stream at the same indicies together.
 // The audio streams will be in the form of a list of
 // integer shorts to represent 16-bit PCM audio.
 // The lists may be of arbitrary length as long as they are
@@ -7,13 +8,23 @@
 const mix = function() {
     // Pull the audio streams from the function arguments object
     var streams = arguments
-    // Loop through the streams and print them
-    for (i = 0; i < streams.length; i++) {
-        console.log(streams[i])
+
+    // Create the output stream of the same length as the inputs
+    var outputStream = new Int16Array(streams[0].length)
+
+    for (i = 0; i < streams[0].length; i++) {
+        for (j = 0; j < streams.length; j++) {
+            // Sum the samples together in the output stream
+            outputStream[i] += streams[j][i]
+        }
     }
+    console.log(outputStream)
+    return outputStream
 }
 
 // 2 audio streams with 2 samples each
-var stream1 = new Int16Array([1, 2])
-var stream2 = new Int16Array([3, 4])
-mix(stream1,stream2)
+var stream1 = new Int16Array([1, 2, 3, 4])
+var stream2 = new Int16Array([1, 2, 3, 4])
+var stream3 = new Int16Array([1, 2, 3, 4])
+
+mix(stream1, stream2, stream3)
